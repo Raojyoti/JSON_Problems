141fabf8dde2d48542ff0e66ba0c9f9a6a44222b
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-28 22:13:03 +0530

    [Jyoti Rao] JONSON:Create Json From Inventory Object

---
+++ JSON_Problems/Operation.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JSON_PROBLEMS
+{
+    public class Operation
+    {
+        public void ReadJsonFile(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            List<Model> data = JsonConvert.DeserializeObject<List<Model>>(json);
+            Console.WriteLine("Name Weight Price Total Value");
+            Console.WriteLine("------------------------------------------------------");
+            foreach (var content in data)
+            {
+                double value = content.Price * content.Weight;
+                Console.WriteLine(content.Name + "   " + content.Weight + "   " + content.Price + "   " + value);
+            }
+        }
+    }
+}
---
+++ JSON_Problems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JSON_PROBLEMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation json = new Operation();
+            json.ReadJsonFile("E:\\JSON_Problems\\JSON_PROBLEMS\\Inventory.json");
+        }
+    }
+}
